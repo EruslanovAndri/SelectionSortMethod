d2f54f3308b8697c6a63006f8d7d8382dcb0a191
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-09 15:58:51 +0300

    fix bubble sort file

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+    }
+}
+
+void SelectionSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+
+}
+
+
+PrintArray(array);
+SelectionSort(array);
+Console.WriteLine();
+PrintArray(array);
+
