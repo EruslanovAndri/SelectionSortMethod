58dc3649f5ee95ab343fa99ce6396c5d560b06b2
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-09 14:46:14 +0300

    add таблица умножения

--- BigONatation/Program.cs
+++ BigONatation/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿// int n = 6;
+// int[] array = new int[n];
+
+// for (int i = 0; i < n; i++)
+// {
+//     array[i] = Convert.ToInt32(Console.ReadLine()); 
+
+// }
+// Console.WriteLine("[" + string.Join("," ,array) + "]");
+// int sum =0;
+// for (int i = 0; i < n; i++)
+// {
+//     sum += array[i];
+// }
+// Console.WriteLine(sum);
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+        matrix[i, j] = (i + 1) * (j + 1);
+        matrix[j, i] = (i + 1) * (j + 1);
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        {
+            Console.Write(matrix[i, j]);
+            Console.Write(" ");
+        }
+
+    }
+    Console.WriteLine();
+}
