86a8829c373b1a9d9c05b0b31a6e07976bcd50b6
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-11 22:32:17 +0300

    add Recursion method countdown int numbers

---
+++ Recursion/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Put the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+RecursionCountDown(n);
+int RecursionCountDown(int n)
+{
+    if (n == 0) return n;
+    {
+        Console.WriteLine($"{n}");
+        RecursionCountDown(n - 1);
+    }
+    return n;
+}
