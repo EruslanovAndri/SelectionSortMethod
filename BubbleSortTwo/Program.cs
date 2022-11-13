2325c06093e85bd6a67f9393a1668609d38ac5c8
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-13 14:25:42 +0300

    add Bubble sort method one more example

---
+++ BubbleSortTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿// Buuble sort
+
+Console.Clear();
+Console.WriteLine("Put the number of array elements:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Put element one by one: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Usorted array = [" + string.Join(",", array) + "]");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - i - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+
+        }
+    }
+    Console.WriteLine("Iteration numbers = " + i + " Sorted array = [" + string.Join(",", array) + "]");
+}
+
+
+
