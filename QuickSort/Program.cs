b97ca0972958b0bdcc53c41e49dd6f98e01ff695
EruslanovAndrei <eruslanov84as@gmail.com>
2022-11-14 14:33:02 +0300

    add Quick sort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,66 @@
+﻿// Quick sort
+Console.Clear();
+Console.WriteLine("введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+void FillArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        var rnd = new Random().Next(-10, 100);
+        for (int j = 0; j < array.Length; j++)
+        {
+            array[i] = rnd;
+        }
+
+    }
+}
+
+void PrintArray(int[] array)
+{
+    var rnd = new Random().Next(1, 100);
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + ",");
+        else Console.WriteLine(array[i] + "]");
+    }
+}
+
+void QuickSort(int[] inputArray, int minIndex, int maxIndex)
+{
+    if (minIndex >= maxIndex) return;
+    int pivot = GetPivotIndex(inputArray, minIndex, maxIndex);
+    QuickSort(inputArray, minIndex, pivot - 1);
+    QuickSort(inputArray, pivot + 1, maxIndex);
+    return;
+}
+
+int GetPivotIndex(int[] inputArray, int minIndex, int maxIndex)
+{
+    int pivotIndex = minIndex - 1;
+    for (int i = minIndex; i <= maxIndex; i++)
+    {
+        if (inputArray[i] < inputArray[maxIndex])
+        {
+            pivotIndex++;
+            Swap(inputArray, i, pivotIndex);
+        }
+    }
+    pivotIndex++;
+    Swap(inputArray, pivotIndex, maxIndex);
+    return pivotIndex;
+}
+
+void Swap(int[] inputArray, int leftValue, int rightValue)
+{
+    int temp = inputArray[leftValue];
+    inputArray[leftValue] = inputArray[rightValue];
+    inputArray[rightValue] = temp;
+}
+FillArray(array);
+PrintArray(array);
+QuickSort(array, 0, array.Length - 1);
+PrintArray(array);
